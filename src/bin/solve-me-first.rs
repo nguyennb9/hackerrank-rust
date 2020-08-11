d519e5603f3dbf0031b775c0453e7e7c9b97edd0
nguyen <nguyen@localhost>
2020-08-11 10:30:17 +0700

    Init repository

---
+++ src/bin/solve-me-first.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut str1 = String::new();
+    let mut str2 = String::new();
+
+    io::stdin().read_line(&mut str1).ok().expect("read error");
+    io::stdin().read_line(&mut str2).ok().expect("read error");
+
+    let num1: i32 = str1.trim().parse().ok().expect("parse error");
+    let num2: i32 = str2.trim().parse().ok().expect("parse error");
+
+    println!("Input number 1: {}", num1);
+    println!("Input number 2: {}", num2);
+
+    println!("{0} + {1} = {2}", num1, num2, num1 + num2);
+}
