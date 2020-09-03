c4643df27fcb8b38e3723e3a716c56c4bc763975
nguyen <nguyen@localhost>
2020-09-03 09:16:00 +0700

    Solved solve-me-first

--- src/bin/solve-me-first.rs
+++ src/bin/solve-me-first.rs
@@ -1,10 +1,16 @@
+/**
+ * Read two numbers from the standard io then add it and print out the result
+ */
 use std::io;
 
 fn main() {
     let mut str1 = String::new();
     let mut str2 = String::new();
 
+    println!("Please enter number 1st:");
     io::stdin().read_line(&mut str1).ok().expect("read error");
+
+    println!("Please enter number 2st:");
     io::stdin().read_line(&mut str2).ok().expect("read error");
 
     let num1: i32 = str1.trim().parse().ok().expect("parse error");
